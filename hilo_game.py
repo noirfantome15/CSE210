c0932d3cd6af28c43f31ce8ae5f7c37d46e59521
blacknight15 <her21071@byui.edu>
2022-10-07 21:49:31 -0600

    1st attempt

--- hilo_game.py
+++ hilo_game.py
@@ -11,23 +11,23 @@ Last updated by 10/07/2022
 
 #Main, will run until game is over
 def main():
-
-    score = score.calculate("Yes")     
+    points = score(300)    
+    points.show()
 
 #tracks the score of the game
 class score:
     def __init__(self, score):
         self.score = score
 
-    @classmethod
-    def calculate(cls, guessed):
+    def calculate(cls, score, guessed):
         if guessed == "Yes":
-            score = score + 100
+            return cls(score = score + 100)
         else:
-            score = score - 70
+            return cls(score = score - 70)
+
+    def show(self):
         print(f"Your score is: {score}")
     
-
 
 # Runs the main function
 if __name__ == "__main__":
