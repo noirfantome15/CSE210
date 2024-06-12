965c8bf28fda569346292379a63b194f008cec37
Dubhhex15 <emanuel.josue.herrera@gmail.com>
2024-06-11 21:32:22 -0600

    python

---
+++ Testing2.py
@@ -0,0 +1,16 @@
+def add(a,b):
+  return a+b
+ 
+#initializing the variables
+num1 = print("what is your first number?")
+num2 = print("what is your second number?")
+ 
+#function calling and store the result into sum_of_twonumbers
+sum_of_twonumbers = add(num1,num2)
+ 
+#To print the result
+print("Sum of {0} and {1} is {2};" .format(num1, num2, sum_of_twonumbers))
+
+
+
+
